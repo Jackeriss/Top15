f09845f5fe8718983d7a541003a3ac4eb5a8804a
Jackeriss <i@jackeriss.com>
2016-09-12 10:15:14 +0800

    [MOD]updateAll.py

--- updateAll.py
+++ updateAll.py
@@ -6,11 +6,7 @@ def updateAll():
     for fileName in os.listdir('data'):
         if os.path.isfile('data/' + fileName):
             args = fileName.strip('.json').split(' ')
-            update_thread = updateItemListThread(user_id=args[0],
-                                                 object_type=args[1],
-                                                 group_type=args[2],
-                                                 order_by=args[3],
-                                                 tag=args[4])
+            update_thread = updateItemListThread(*args)
             update_thread.start()
 
 if __name__ == '__main__':
