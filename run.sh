479c20bc41d443da4941f956958845360706cacb
Jackeriss <i@jackeriss.com>
2017-02-26 08:06:06 +0800

    [FIX]

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 cron
 python3 application.py
-python3 updateAll.py
--- updateAll.py
+++ updateAll.py
@@ -1,14 +1,11 @@
 import os
-import time
 from app import spider
 
 def updateAll():
-    while 1:
-        for fileName in os.listdir('data'):
-            if os.path.isfile('data/' + fileName):
-                args = fileName.strip('.json').split(' ')
-                spider.grab(*args)
-        time.sleep(86400)
+    for fileName in os.listdir('data'):
+        if os.path.isfile('data/' + fileName):
+            args = fileName.strip('.json').split(' ')
+            spider.grab(*args)
 
 if __name__ == '__main__':
     updateAll()
