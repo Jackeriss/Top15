60011ca214f2c0a592705c1e3ea35992e3cbdbf5
Jackeriss <i@jackeriss.com>
2017-05-14 23:21:35 +0800

    [FIX]update cookie

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ COMMON_CONFIG = {
     'DATA_DIR': os.path.join(BASEDIR, 'data'),
     'COOKIE_SECRET': 'oqA/6vVxSu6IU+5UErK21/yv7XHASUwap+0z6WL3TJQ=',
     'HEADERS': {'cookie': ('ll="118348"; bid=u7DQ-3A5QCo; ps=y; ue="1075704670'
-                           '@qq.com"; dbcl2="63626550:zAz6Nxzch4M"; ck=B1MZ; a'
-                           'p=1; push_noty_num=0; push_doumail_num=0')},
+                           '@qq.com"; dbcl2="63626550:zAz6Nxzch4M"; ck=B1MZ; c'
+                           't=y; ap=1; push_noty_num=0; push_doumail_num=0')},
     'REDIS_HOST': 'localhost',
     'REDIS_PORT': 6379}
 
