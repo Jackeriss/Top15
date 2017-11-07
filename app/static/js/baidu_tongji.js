b6711b0518213a182dfe258954c29db4df45470d
Zhao Yufei <zhaoyufei@newdun.com>
2017-11-07 11:28:49 +0800

    Fix

--- app/static/js/baidu_tongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?aa41606cb54089d4ed516bf2e6a94ecf";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
